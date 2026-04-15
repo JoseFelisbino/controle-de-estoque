5930d74097c1b65c238e3f77a5d4fed2d4c314db
JoseFelisbino <lu31660@gmail.com>
2026-04-15 05:18:33 -0300

    fix: ajuste no nome do arquivo principal e na api

--- frontend/js/services/api.js
+++ frontend/js/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://controle-de-estoque-tp7j.onrender.com";
 
 export async function registerRequest(data) {
   const res = await fetch(`${BASE_URL}/auth/register`, {
